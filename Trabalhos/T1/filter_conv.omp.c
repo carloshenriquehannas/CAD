1141735e674af227a8b20f0a0982612d987d27cf
Lucas Stapf <lucas.c.stapf@gmail.com>
2024-10-08 08:28:16 -0300

    Adicionado código para medir tempo

--- Trabalhos/T1/filter_conv.omp.c
+++ Trabalhos/T1/filter_conv.omp.c
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <time.h>
 
 /// Define a quantidade de threads a serem criadas pelo OMP.
 #define NUM_THREADS 4
@@ -27,11 +28,16 @@
 /// c: número de colunas da matriz.
 #define INDEX(i, j, c) ((i * c) + j)
 
+/// Calcula a linha i de uma matriz que está representada na forma de um vetor com base no índice n.
 #define I(n, c) (n / c)
+
+/// Calcula a coluna j de uma matriz que está representada na forma de um vetor com base no índice n.
 #define J(n, c) (n % c)
 
 int main(int argc, char const *argv[])
 {
+    // Remover depois
+    clock_t time;
     
     if (argc != 4)
     {
@@ -49,40 +55,37 @@ int main(int argc, char const *argv[])
         img[n] = rand() % 256;
     
     // TESTE - REMOVER DPS
-    printf("NxN:\n");
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf("%d ", img[INDEX(i, j, N)]);
-        printf("\n");
-    }
-    printf("\n");
+    // printf("NxN:\n");
+    // for (int i = 0; i < N; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //         printf("%d ", img[INDEX(i, j, N)]);
+    //     printf("\n");
+    // }
+    // printf("\n");
     // FIM DO TESTE - REMOVER DPS
     
     // Criação do filtro
     float *filter = malloc(M * M * sizeof(float));
     
     for (int m = 0; m < M * M; m++)
-    {
         filter[m] = (rand() % 10) / 10.0;
-        // float v = ((float) rand()) / (RAND_MAX);
-        // filter[m] = ((int) (v * 10)) / 10.0;
-    }
 
     // TESTE - REMOVER DPS
-    printf("MxM\n");
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < M; j++)
-            printf("%0.1f ", filter[INDEX(i, j, M)]);
-        printf("\n");
-    }
-    printf("\n");
+    // printf("MxM\n");
+    // for (int i = 0; i < M; i++)
+    // {
+    //     for (int j = 0; j < M; j++)
+    //         printf("%0.1f ", filter[INDEX(i, j, M)]);
+    //     printf("\n");
+    // }
+    // printf("\n");
     // FIM DO TESTE - REMOVER DPS
 
     // Criação da imagem nova (Só para testes)
     unsigned char *new_img = malloc(N * N * sizeof(unsigned char));
 
+    time = clock();
     unsigned char maior = 0, menor = 255;
     #pragma omp parallel for num_threads(NUM_THREADS) reduction(max: maior) reduction(min: menor)
     for (int n = 0; n < N * N; n++)
@@ -117,14 +120,18 @@ int main(int argc, char const *argv[])
     }
 
     // TESTE - REMOVER DPS
-    printf("NxN * MxM:\n");
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf("%d ", new_img[INDEX(i, j, N)]);
-        printf("\n");
-    }
-    printf("\n");
+    time = clock() - time;
+    printf("Tempo: %fs\n", ((double) time) / (CLOCKS_PER_SEC));
+
+    // TESTE - REMOVER DPS
+    // printf("NxN * MxM:\n");
+    // for (int i = 0; i < N; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //         printf("%d ", new_img[INDEX(i, j, N)]);
+    //     printf("\n");
+    // }
+    // printf("\n");
     // FIM DO TESTE - REMOVER DPS
 
     printf("%d %d\n", maior, menor);
