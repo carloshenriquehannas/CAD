1edc71752385ab1b4981c9caad2b5bce580ce845
Carlos Henrique Hannas de Carvalho <carloshenriquehannas@gmail.com>
2024-10-10 15:41:26 -0300

    Remocao de prints de teste

--- Trabalhos/T1/filter_conv.omp.c
+++ Trabalhos/T1/filter_conv.omp.c
@@ -16,76 +16,46 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
-#include <time.h>
 
-/// Define a quantidade de threads a serem criadas pelo OMP.
+/// Define a quantidade de threads a serem criadas pelo OMP
 #define NUM_THREADS 8
 
-/// Calcula o índice linear equivalente de uma matriz bidimensional armazenada 
-/// em um array unidimensional, como se fosse uma matriz armazenada de forma contínua em memória.
-/// Parâmetros:
-/// (i, j): índices da linha e da coluna da matriz.
-/// c: número de colunas da matriz.
+/// Calcula o indice linear equivalente de uma matriz bidimensional armazenada 
+/// em um array unidimensional, como se fosse uma matriz armazenada de forma continua em memoria
+/// Parametros:
+/// (i, j): indices da linha e da coluna da matriz.
+/// c: numero de colunas da matriz. 
 #define INDEX(i, j, c) ((i * c) + j)
 
-/// Calcula a linha i de uma matriz que está representada na forma de um vetor com base no índice n.
+/// Calcula a linha i de uma matriz que esta representada na forma de um vetor com base no indice n.
 #define I(n, c) (n / c)
 
-/// Calcula a coluna j de uma matriz que está representada na forma de um vetor com base no índice n.
+/// Calcula a coluna j de uma matriz que esta representada na forma de um vetor com base no indice n.
 #define J(n, c) (n % c)
 
 int main(int argc, char const *argv[])
 {
-    // Remover depois
-    clock_t time;
-    
-    if (argc != 4)
-    {
-        printf("Numero invalido de argumentos\n");
-        return 1;
-    }
 
-    int N = atoi(argv[1]), M = atoi(argv[2]), S = atoi(argv[3]);
+    int N, M, S;
+    scanf("%d %d %d", &N, &M, &S);
+
     srand(S);
 
-    // Criação da imagem original
+    // Criacao da imagem original
     unsigned char *img = malloc(N * N * sizeof(unsigned char));
-    
     for (int n = 0; n < N * N; n++)
         img[n] = rand() % 256;
     
-    // TESTE - REMOVER DPS
-    // printf("NxN:\n");
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //         printf("%d ", img[INDEX(i, j, N)]);
-    //     printf("\n");
-    // }
-    // printf("\n");
-    // FIM DO TESTE - REMOVER DPS
+
     
-    // Criação do filtro
+    // Criacao do filtro
     float *filter = malloc(M * M * sizeof(float));
-    
     for (int m = 0; m < M * M; m++)
         filter[m] = (rand() % 10) / 10.0;
 
-    // TESTE - REMOVER DPS
-    // printf("MxM\n");
-    // for (int i = 0; i < M; i++)
-    // {
-    //     for (int j = 0; j < M; j++)
-    //         printf("%0.1f ", filter[INDEX(i, j, M)]);
-    //     printf("\n");
-    // }
-    // printf("\n");
-    // FIM DO TESTE - REMOVER DPS
-
-    // Criação da imagem nova (Só para testes)
+    // Criação da imagem nova ( para testes)
     unsigned char *new_img = malloc(N * N * sizeof(unsigned char));
 
-    time = clock();
     unsigned char maior = 0, menor = 255;
     #pragma omp parallel for num_threads(NUM_THREADS) reduction(max: maior) reduction(min: menor)
     for (int n = 0; n < N * N; n++)
@@ -107,8 +77,6 @@ int main(int argc, char const *argv[])
             }
         }
         
-        // printf("%f\n", sum_prod);
-        // unsigned char result = ((int) sum_prod <= 255.0) ? (unsigned char) sum_prod : 255;
         unsigned char result = (sum_prod > 255) ? 255 : sum_prod;
         if (result > maior)
             maior = result;
@@ -118,21 +86,6 @@ int main(int argc, char const *argv[])
 
         new_img[n] = result;
     }
-
-    // TESTE - REMOVER DPS
-    time = clock() - time;
-    printf("Tempo: %fs\n", ((double) time) / (CLOCKS_PER_SEC));
-
-    // TESTE - REMOVER DPS
-    // printf("NxN * MxM:\n");
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //         printf("%d ", new_img[INDEX(i, j, N)]);
-    //     printf("\n");
-    // }
-    // printf("\n");
-    // FIM DO TESTE - REMOVER DPS
 
     printf("%d %d\n", maior, menor);
 
